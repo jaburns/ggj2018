b981a069bf9ac19d2a4dd7bb6655d7278c348e74
Jeremy Burns <j@jaburns.net>
2018-01-26 22:00:10 -0700

    Makin the game

---
+++ Assets/Scripts/CellController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class CellController : MonoBehaviour 
+{
+    int hoverFrames;
+    Rigidbody2D rb;
+
+    public List<CellController> attraction = new List<CellController>();
+    public bool Selected { get; set; }
+    public Vector3? seekPoint;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        attraction = (PlayerController.AllCells.Clone() as CellController[]).ToList();
+        attraction.Remove(this);
+    }
+
+    public void NotifyMouseOver()
+    {
+        if (Input.GetMouseButton(0)) {
+            Selected = true;
+        } else {
+            hoverFrames = 3;
+        }
+    }
+
+    public void PurgeFromAttraction(List<CellController> cells) 
+    {
+        foreach (var cell in cells) {
+            attraction.Remove(cell);
+        }
+    }
+
+    void Update()
+    {
+        if (hoverFrames > 0) {
+            hoverFrames--;
+        }
+
+        if (Selected) {
+            GetComponentInChildren<Renderer>().material.color = Color.red;
+        } else if (hoverFrames > 0) {
+            GetComponentInChildren<Renderer>().material.color = Color.yellow;
+        } else {
+            GetComponentInChildren<Renderer>().material.color = Color.white;
+        }
+    }
+
+    static Vector2 forceBetweenCells(CellController thisCell, CellController otherCell)
+    {
+        var ds = otherCell.transform.position - thisCell.transform.position;
+        return ds.normalized / ds.magnitude;
+    }
+
+    void FixedUpdate()
+    {
+        foreach (var cell in attraction) {
+            rb.AddForce(forceBetweenCells(this, cell));
+        }
+
+        if (seekPoint.HasValue) {
+            rb.AddForce(1f * (seekPoint.Value - transform.position).normalized);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour 
+{
+    static public CellController[] AllCells { get; private set; }
+
+    [SerializeField] GameObject cellPrefab;
+
+    void Awake()
+    {
+        AllCells = generateCells();
+    }
+
+    CellController[] generateCells()
+    {
+        var cellList = new List<CellController>();
+        for (int i = 0; i < 100; ++i) {
+            var cellObj = Instantiate(cellPrefab, transform.position + Vector3.right * Random.Range(-10f, 10f) + Vector3.up * Random.Range(-10f, 10f), Quaternion.identity) as GameObject;
+            cellList.Add(cellObj.GetComponent<CellController>());
+        }
+        return cellList.ToArray();
+    }
+
+    void Update() 
+    {
+        bool hitCell = false;
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f, 1 << LayerMask.NameToLayer("MouseInteract"));
+        foreach (var hit in hits) {
+            if (hit.collider != null) {
+                var cell = hit.collider.transform.parent.GetComponent<CellController>();
+                if (cell != null) {
+                    hitCell = true;
+                    cell.NotifyMouseOver();
+                }
+            }
+        }
+
+        var selectedCells = new List<CellController>();
+        var unselectedCells = new List<CellController>();
+
+        if (Input.GetMouseButtonDown(0) && !hitCell) {
+            foreach (var cell in AllCells) {
+                if (cell.Selected) selectedCells.Add(cell);
+                else unselectedCells.Add(cell);
+            }
+
+            foreach (var cell in selectedCells) {
+                cell.attraction.Clear();
+                cell.attraction.AddRange(selectedCells);
+                cell.attraction.Remove(cell);
+
+                cell.Selected = false;
+
+                cell.seekPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+
+            foreach (var cell in unselectedCells) {
+                cell.PurgeFromAttraction(selectedCells);
+
+                cell.seekPoint = null;
+            }
+        }
+	}
+}
