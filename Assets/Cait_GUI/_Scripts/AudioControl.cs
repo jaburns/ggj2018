d28d493b0af162621732c3370b686df772c43433
Jeremy Burns <j@jaburns.net>
2018-01-27 22:13:48 -0700

    Added Cait's GUI assets

---
+++ Assets/Cait_GUI/_Scripts/AudioControl.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class AudioControl : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+    public string masterVolume = "MasterVolume";
+    public string musicVolume = "MusicVolume";
+    public string SFXVolume = "SFXVolume";
+
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+
+    public void loadExisting()
+    {
+
+        float existingMaster = PlayerPrefs.GetFloat(masterVolume, 1f);
+        float existingMusic = PlayerPrefs.GetFloat(musicVolume, 1f);
+        float existingSFX = PlayerPrefs.GetFloat(SFXVolume, 1f);
+
+
+        SetMasterLevel(existingMaster);
+        SetMusicLevel(existingMusic);
+        SetSfxLevel(existingSFX);
+
+        masterSlider.value = existingMaster;
+        musicSlider.value = existingMusic;
+        sfxSlider.value = existingSFX;
+    }
+
+    private void OnEnable()
+    {
+        loadExisting();
+
+    }
+
+    private void OnDisable()
+    {
+        float newMaster;
+        float newMusic;
+        float newSFX;
+
+        masterMixer.GetFloat(masterVolume, out newMaster);
+        masterMixer.GetFloat(musicVolume, out newMusic);
+        masterMixer.GetFloat(SFXVolume, out newSFX);
+
+        newMaster = convertDBToFLoat(newMaster);
+        newMusic = convertDBToFLoat(newMusic);
+        newSFX = convertDBToFLoat(newSFX);
+
+        PlayerPrefs.SetFloat(masterVolume, newMaster);
+        PlayerPrefs.SetFloat(musicVolume, newMusic);
+        PlayerPrefs.SetFloat(SFXVolume, newSFX);
+    }
+
+
+
+    /*convert d8 to float
+     * 10^(x/20)-y
+     */
+    float convertDBToFLoat(float dB)
+    {
+        float volume = 0;
+
+        //x/20
+
+        //volume = x/20
+        volume = dB / 20;
+        // volume = 10 ^ volume
+        volume = Mathf.Pow(10f, volume);
+
+
+        return volume;
+
+    }
+
+    float convertFloatToDB(float volume)
+    {
+        float dB = 20 * Mathf.Log10(volume); ;
+
+
+        return dB;
+    }
+
+    // Ensure each variable eg. "masterVol" is named EXACTLY right to what you exposed the variable names as.
+    public void SetMasterLevel(float MasterLevel)
+    {
+        MasterLevel = convertFloatToDB(MasterLevel);
+        masterMixer.SetFloat("MasterVolume", MasterLevel);
+    }
+
+    public void SetSfxLevel(float sfxLevel)
+    {
+        sfxLevel = convertFloatToDB(sfxLevel);
+        masterMixer.SetFloat("SFXVolume", sfxLevel);
+    }
+
+    public void SetMusicLevel(float musicLevel)
+    {
+        musicLevel = convertFloatToDB(musicLevel);
+        masterMixer.SetFloat("MusicVolume", musicLevel);
+    }
+
+}
+
---
+++ Assets/Cait_GUI/_Scripts/GUIManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+
+public class GUIManager : MonoBehaviour {
+
+    public GameObject scrMainMenu;
+    public GameObject scrOptions;
+    public GameObject scrGamePlay;
+    public GameObject scrPause;
+    public GameObject scrWin;
+    public GameObject scrLose;
+
+    public AudioControl audioControl;
+
+   
+    void Start () {
+
+        scrMainMenu.SetActive(true);
+        scrOptions.SetActive(false);
+        scrGamePlay.SetActive(false);
+        scrPause.SetActive(false);
+
+        audioControl.loadExisting();
+
+    }
+	
+
+	void Update () {
+
+		
+	}
+
+    
+
+    public void GoToGame()
+    {
+        scrMainMenu.SetActive(false);
+        scrPause.SetActive(false);
+        scrGamePlay.SetActive(true);
+    }
+
+    public void GoToPause()
+    {
+        scrPause.SetActive(true);
+    }
+
+    public void GoToOptions()
+    {
+        scrOptions.SetActive(true);
+    }
+
+    public void GoToWin()
+    {
+        scrGamePlay.SetActive(false);
+        scrWin.SetActive(true);
+    }
+
+    public void GoToLose()
+    {
+        scrGamePlay.SetActive(false);
+        scrLose.SetActive(true);
+    }
+}
