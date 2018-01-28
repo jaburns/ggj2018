adb4cd437c75a32b9d7ee92a347687f0e8b36495
Jeremy Burns <j@jaburns.net>
2018-01-28 13:59:33 -0700

    Finished the title screen (sans music)

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Pulse : MonoBehaviour 
+{
+    Vector3 startScale;
+
+	void Start () 
+    {
+        startScale = transform.localScale;
+	}
+
+	void Update() 
+    {
+        transform.localScale = startScale * (1f + .1f * Mathf.Sin(2 * Time.time));
+	}
+}
